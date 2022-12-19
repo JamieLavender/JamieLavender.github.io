92de0747c8f65596f02c74998769e53885e6a3f7
Jamie Lavender <lavenderjamie0@gmail.com>
2022-12-20 09:49:00 +1100

    v0.8.26

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-
+function navToggle() {
+  var navBar = document.getElementById("navBar");
+  if (navBar.className === "nav") {
+    navBar.className += " responsive";
+  } else {
+    navBar.className = "nav";
+  }
+}
